503483c4487db8eb53c6a059566bc66a5e4bf273
jessia <sadiajessia@gmail.com>
2024-04-26 22:28:35 +0600

    performing CRUD operation

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,25 @@ router.get('/new', (req, res)=> {
     res.send('new user form');
   });
 
+  // to create
+router.post('/', (req, res)=> {
+    res.send('create user');
+  });
+
+  router.get('/:id', (req, res)=> {  //number directly from the URL
+    // req.params.id; 
+    //dynamic parameter
+    res.send('Get User With ID ' +req.params.id);
+  });
+
+  // to update
+  router.post('/', (req, res)=> {
+    res.send('update user with ID ' +req.params.id);
+  });
+
+  //to delete
+  router.post('/', (req, res)=> {
+    res.send('delete user with ID ' +req.params.id);
+  });
+
   module.exports = router
