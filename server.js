29b2e982bf0f994ccfe370bf2745cd4442ce2c09
jessia <sadiajessia@gmail.com>
2024-04-26 20:50:05 +0600

    ejs added

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res)=> {
   console.log("Here");
-  res.send('Hi');
-  res.download("server.js");
+  // res.send('Hi');
+  // res.download("server.js");
+  res.render('index');
 });
 
 app.listen(3000);
